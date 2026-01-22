c296de1e3e8b163efade1865be81fc7f0e82f02c
Mohammed Ashil <muhammedashil7@gmail.com>
2026-01-22 19:40:45 +0530

    day 3 and 4 log,status, import and export functions done

--- cli/main.py
+++ cli/main.py
@@ -8,6 +8,11 @@ from core.status import get_status
 from core.repo import init_repo
 from core.index import add_files
 from core.commit import make_commit
+from core.log import get_log
+from core.undo import undo_last_commit
+from core.exporter import export_repo
+from core.importer import import_repo
+
 
 if len(sys.argv) < 2:
     print("Usage: vcs <command>")
@@ -45,6 +50,34 @@ elif command == "status":
         print("🟢 New files:", result["new"])
         print("🟡 Modified files:", result["modified"])
         print("🔴 Deleted files:", result["deleted"])
+
+elif command == "log":
+    result = get_log()
+    if not result["success"]:
+        print(result["message"])
+    else:
+        for c in result["commits"]:
+            print(f"commit {c['id']}")
+            print(f"    {c['message']}")
+            print(f"    {c['timestamp']}\n")
+
+elif command == "undo":
+    result = undo_last_commit()
+    print(result["message"])
+
+elif command == "export":
+    if len(sys.argv) < 3:
+        print("Usage: vcs export <file.vcs>")
+    else:
+        result = export_repo(sys.argv[2])
+        print(result["message"])
+
+elif command == "import":
+    if len(sys.argv) < 3:
+        print("Usage: vcs import <file.vcs>")
+    else:
+        result = import_repo(sys.argv[2])
+        print(result["message"])
 
 
 else:
